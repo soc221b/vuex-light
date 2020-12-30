36b24352ea412eb89fb5b54756269ab645f3f570
Ernest <iendeavor.github.io@gmail.com>
2020-12-30 22:10:07 +0800

    refactor: extract default key

--- src/install.ts
+++ src/install.ts
@@ -1,5 +1,7 @@
 import { App } from 'vue'
 
+const defaultStoreKey = '$store'
 export function install<Store>(app: App, { store, storeKey }: { store: Store; storeKey?: string }) {
-  app.config.globalProperties[storeKey || '$store'] = store
+  const theStoreKey = storeKey || defaultStoreKey
+  app.config.globalProperties[theStoreKey] = store
 }
