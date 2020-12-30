9e07590e7995798062484a3430a90a0b10afad97
Ernest <iendeavor.github.io@gmail.com>
2020-12-30 21:56:00 +0800

    docs: init example

---
+++ example/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  setup() {
+    return {
+      count: ref(0),
+    }
+  },
+}
+</script>
