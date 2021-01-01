6f69599c0eb0504dc84c1cdad10a7a07d0623dc1
Ernest <iendeavor.github.io@gmail.com>
2021-01-02 01:35:52 +0800

    chore: expose some arguments for build

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,10 @@ const rimraf = require('rimraf')
 const execa = require('execa')
 
 rimraf.sync(path.resolve(__dirname, '..', 'dist'))
-execa.sync('rollup', ['-c', 'rollup.config.js', ...process.argv.slice(2)], { stdio: 'inherit' })
-execa.sync('api-extractor', ['run'], { stdio: 'inherit' })
+execa.sync('rollup', ['-c', 'rollup.config.js'], { stdio: 'inherit' })
+execa.sync('api-extractor', ['run', ...getArguments('-l', '--local')], { stdio: 'inherit' })
 rimraf.sync(path.resolve(__dirname, '..', 'dist', 'src'))
+
+function getArguments(...argvs) {
+  return argvs.filter(argv => process.argv.includes(argv))
+}
